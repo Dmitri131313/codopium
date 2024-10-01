42c3da33f276241b20bbc32dbce052abd6da7797
Sergey Kulikov <bin_rob@mail.ru>
2024-10-02 02:52:21 +0700

    Enable Angular Hash Routing. Create OptionsComponent, PopupComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'options',
+  },
+  {
+    path: 'popup',
+    loadChildren: () => import('./components/popup/popup.routes').then(c => c.routes)
+  },
+  {
+    path: 'options',
+    loadChildren: () => import('./components/options/options.routes').then(c => c.routes)
+  }
+];
