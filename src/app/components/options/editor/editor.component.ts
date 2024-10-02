ad85b728ce6142a6c591c07043c00bc283e73751
Sergey Kulikov <bin_rob@mail.ru>
2024-10-03 01:42:47 +0700

    Integrate EditorComponent into OptionsComponent

--- src/app/components/options/editor/editor.component.ts
+++ src/app/components/options/editor/editor.component.ts
@@ -1,10 +1,16 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 import {Ace} from "ace-builds"
 import * as ace from 'ace-builds/src-noconflict/ace'
 import "ace-builds/src-noconflict/ext-language_tools"
 import "ace-builds/src-noconflict/snippets/javascript"
 import "ace-builds/src-noconflict/snippets/css"
+import {FormGroupDirective} from "@angular/forms";
+
+export enum EditorType {
+  CSS = 'css',
+  JS = 'javascript'
+}
 
 @Component({
   selector: 'app-editor',
@@ -13,11 +19,34 @@ import "ace-builds/src-noconflict/snippets/css"
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
-export class EditorComponent implements AfterViewInit {
+export class EditorComponent implements AfterViewInit, OnInit {
+
+  @Input({required: true}) type!: EditorType
   @ViewChild('editor') private editorElement: ElementRef<HTMLElement> | undefined
+
+  private editor: Ace.Editor | undefined
+  private ifBlockEditorChangeListener: boolean = false
+  private ifBlockFormChangeListener: boolean = false
+
+  constructor(
+    private formGroupDirective: FormGroupDirective,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.formGroupDirective.form.get(this.isJSCode() ? 'js' : 'css')?.valueChanges.subscribe(this.loadCode.bind(this))
+  }
 
   ngAfterViewInit(): void {
     this.initEditor()
+  }
+
+  private loadCode(code: string): void {
+    if (!this.ifBlockFormChangeListener) {
+      this.ifBlockEditorChangeListener = true
+      this.setEditorValue(code)
+      this.ifBlockEditorChangeListener = false
+    }
   }
 
   private initEditor(): void {
@@ -26,6 +55,52 @@ export class EditorComponent implements AfterViewInit {
       ace.config.set('basePath', 'ace');
       // This option is required for correct work as a Chrome extension
       ace.config.set("loadWorkerFromBlob", false)
+      const mode = 'ace/mode/' + this.type.toString()
+      const placeholder = 'Start typing ' + (this.isJSCode() ? 'Javascript' : 'CSS') + ' here...'
+      this.editor = ace.edit(this.editorElement.nativeElement, {
+        mode,
+        placeholder,
+      })
+      this.editor?.setOptions({
+        //enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true,
+        wrap: true,
+        showPrintMargin: false,
+        useSoftTabs: false,
+        fontSize: 16,
+      })
+      this.bindEditorEvents()
     }
+  }
+
+  private bindEditorEvents() {
+    if (this.editor) {
+      this.editor.session.on('change', this.onChanged.bind(this))
+    }
+  }
+
+  private onChanged() {
+    if (!this.ifBlockEditorChangeListener) {
+      const formControlName = this.isJSCode() ? 'js' : 'css'
+      this.ifBlockFormChangeListener = true
+      this.formGroupDirective.form.patchValue({
+        [formControlName]: this.getEditorValue(),
+      })
+      this.ifBlockFormChangeListener = false
+      this.formGroupDirective.form.get(formControlName)?.markAsDirty()
+    }
+  }
+
+  private getEditorValue(): string {
+    return this.editor?.session.getValue() ?? ''
+  }
+
+  private setEditorValue(code: string): void {
+    this.editor?.session.setValue(code)
+  }
+
+  private isJSCode(): boolean {
+    return this.type === EditorType.JS
   }
 }
--- src/app/components/options/options/options.component.ts
+++ src/app/components/options/options/options.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {EditorComponent, EditorType} from "../editor/editor.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-options',
   standalone: true,
-  imports: [],
+  imports: [
+    EditorComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './options.component.html',
   styleUrl: './options.component.scss'
 })
 export class OptionsComponent {
 
+  codeBundleForm = new FormGroup({
+    id: new FormControl<string>(''),
+    urlPatternsCommaSeparated: new FormControl<string>('', [Validators.required]),
+    js: new FormControl<string>(''),
+    css: new FormControl<string>(''),
+  })
+
+  protected readonly EditorType = EditorType;
+
+  test() {
+    this.codeBundleForm.patchValue({
+      js: 'js code',
+      css: 'css code',
+    })
+  }
 }
