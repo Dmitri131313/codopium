71a2652fda3fc4136c790d3f8cc13609fe78628f
Sergey Kulikov <bin_rob@mail.ru>
2024-11-03 01:48:58 +0700

    Add custom storage for script bundles (it duplicates chrome.userScripts, but allow developing extra features)

--- src/app/shared/services/code.service.ts
+++ src/app/shared/services/code.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {ChromeService} from "./chrome.service";
+import {Subject} from "rxjs";
 
 export interface CodeBundle {
   id: string
@@ -14,6 +15,8 @@ export interface CodeBundle {
 })
 export class CodeService {
 
+  onCodeBundlesUpdate: Subject<void> = new Subject<void>()
+
   constructor(
     private chromeService: ChromeService,
   ) { }
@@ -25,14 +28,58 @@ export class CodeService {
     return Promise.all([
       this.chromeService.saveJSToChromeRegisteredScriptsAsync(codeBundle),
       this.chromeService.saveCSSToChromeRegisteredScriptsAsync(codeBundle),
+      this.saveToLocalStorage(codeBundle),
     ])
       .then(() => codeBundle.id)
+  }
+
+  getCodeBundlesAsync(): Promise<CodeBundle[]> {
+    return chrome.storage.local.get({codeBundles: []}).then(items => {
+      const codeBundles: CodeBundle[] = items['codeBundles']
+      this.addAdditionalPropertiesToCodeBundles(codeBundles)
+      return codeBundles
+    })
   }
 
   getUrlPatternsFromCommaSeparatedString(urlPatternsCommaSeparated: string): string[] {
     let urlPatterns: string[] = urlPatternsCommaSeparated.split(',')
     urlPatterns = urlPatterns.map(urlPattern => urlPattern.trim())
     return urlPatterns
+  }
+
+  private addAdditionalPropertiesToCodeBundles(codeBundles: CodeBundle[]): void {
+    codeBundles.forEach((codeBundle: CodeBundle): CodeBundle => {
+      codeBundle.urlPatternsCommaSeparated = codeBundle.urlPatterns.join(', ')
+      return codeBundle
+    })
+  }
+
+  private saveToLocalStorage(codeBundle: CodeBundle): Promise<void> {
+    return this.getCodeBundlesAsync().then((codeBundles: CodeBundle[]) => {
+      codeBundles = this.getCodeBundlesAfterAddOrUpdate(codeBundles, codeBundle)
+      return chrome.storage.local.set({codeBundles}).then(
+        this.notifyCodeBundlesUpdated.bind(this)
+      )
+    })
+  }
+
+  private notifyCodeBundlesUpdated() {
+    this.onCodeBundlesUpdate.next()
+  }
+
+  private getCodeBundlesAfterAddOrUpdate(codeBundles: CodeBundle[], newOrUpdatedCodeBundle: CodeBundle): CodeBundle[] {
+    const ifUpdated: boolean = codeBundles.some((codeBundle: CodeBundle, codeBundleIndex: number): boolean => {
+      if (codeBundle.id === newOrUpdatedCodeBundle.id) {
+        codeBundles[codeBundleIndex] = newOrUpdatedCodeBundle
+        return true
+      } else {
+        return false
+      }
+    })
+    if (!ifUpdated) {
+      codeBundles.push(newOrUpdatedCodeBundle)
+    }
+    return codeBundles
   }
 
   private getNewId(): string {
